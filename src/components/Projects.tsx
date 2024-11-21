c55586dee7e7af8ed4376e8e2748d84749070f85
Mahesh77r <shravanrohane19@gmail.com>
2024-11-21 16:57:54 +0530

    #1 sidebar issue solved

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,7 +4,7 @@ import Projectcards from "./Projectcards";
 const Projects=()=>{
     return(
         
-        <div className="px-16 mx-20 my-20 font-mono md-mx:px-6">
+        <div className="px-16 mx-20 my-20 font-mono md-mx:px-6" id="Projects">
             <br></br><br></br>
             <h1 className="text-4xl  sm-mx:text-3xl xs-mx:text-2xl mb-10 font-bold text-center text-white"><span className="!text-primaryColor">02.&nbsp;</span>Projects</h1>
             <div className="flex flex-wrap justify-around md-mx:justify-between sm-mx:justify-center gap-4 md-mx:gap-2">
